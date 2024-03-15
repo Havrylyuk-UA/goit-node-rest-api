9c659f4bd79de6c0af8433a32f46300ff00692b6
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2024-03-15 16:42:53 +0200

    done?

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,68 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from '../services/contactsServices.js';
 
-export const getAllContacts = (req, res) => {};
+import HttpError from '../helpers/HttpError.js';
 
-export const getOneContact = (req, res) => {};
+import ctrlWrapper from '../decorators/ctrlWrapper.js';
 
-export const deleteContact = (req, res) => {};
+const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
 
-export const createContact = (req, res) => {};
+  res.json(result);
+};
 
-export const updateContact = (req, res) => {};
+const getOneContact = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await contactsService.getContactById(id);
+
+  if (!result) {
+    throw HttpError(404, `Not found`);
+  }
+
+  res.json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { id } = req.params;
+
+  const result = await contactsService.removeContact(id);
+
+  if (!result) {
+    throw HttpError(404, 'Not found');
+  }
+
+  res.json({
+    message: 'Delete success',
+  });
+};
+
+const createContact = async (req, res) => {
+  const { name, email, phone } = req.body;
+  const result = await contactsService.addContact({ name, email, phone });
+
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+
+  if (Object.keys(req.body).length === 0) {
+    throw HttpError(400, `Body must have at least one field`);
+  }
+
+  const result = await contactsService.updateContactById(id, req.body);
+
+  if (!result) {
+    throw HttpError(404, `Not found`);
+  }
+
+  res.json(result);
+};
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+};
---
+++ services/contactsServices.js
@@ -0,0 +1,56 @@
+import { nanoid } from 'nanoid';
+import fs from 'fs/promises';
+import path from 'path';
+
+const contactsPath = path.join('db', 'contacts.json');
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+export const getContactById = async contactId => {
+  const data = await listContacts();
+  const contactById = data.find(contact => contact.id === contactId);
+  return contactById || null;
+};
+
+export const removeContact = async contactId => {
+  const data = await listContacts();
+  const index = data.findIndex(contact => contact.id === contactId);
+
+  if (index === -1) {
+    return null;
+  }
+  const [result] = data.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return result;
+};
+
+export const addContact = async data => {
+  const contacts = await listContacts();
+
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+export const updateContactById = async (id, data) => {
+  const contact = await listContacts();
+
+  const index = contact.findIndex(item => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+
+  contact[index] = { ...contact[index], ...data };
+
+  await fs.writeFile(contactsPath, JSON.stringify(contact, null, 2));
+
+  return contact[index];
+};
